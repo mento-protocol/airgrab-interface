c9a98f9d7bc2f8fc72f99581341abb71569fbe09
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-10-02 11:35:05 -0500

    fix: improve error handling in kyc redirect route

--- src/app/api/kyc/route.ts
+++ src/app/api/kyc/route.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "development") {
   if (!BASE_URL) throw new Error("BASE_URL is not set");
 } else {
   // preview & production
-  BASE_URL = 'https://' + process.env.VERCEL_BRANCH_URL;
+  BASE_URL = "https://" + process.env.VERCEL_BRANCH_URL;
   if (!BASE_URL) throw new Error("VERCEL_BRANCH_URL is not set");
 }
 
@@ -23,19 +23,24 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const wallet = searchParams.get("wallet");
 
-  if (!wallet) {
-    throw new Error("Wallet not connected");
+  try {
+    if (!wallet) {
+      throw new Error("Wallet not connected");
+    }
+
+    const url = new URL(FRACTAL_APP_URL + "/authorize");
+    url.searchParams.append("client_id", FRACTAL_CLIENT_ID!);
+    url.searchParams.append("redirect_uri", REDIRECT_URL);
+    url.searchParams.append("ensure_wallet", wallet);
+    url.searchParams.append("response_type", "code");
+    url.searchParams.append(
+      "scope",
+      "contact:read verification.basic:read verification.basic.details:read verification.liveness:read verification.liveness.details:read verification.wallet-eth:read"
+    );
+
+    return NextResponse.redirect(url);
+  } catch (error) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-
-  const url = new URL(FRACTAL_APP_URL + "/authorize");
-  url.searchParams.append("client_id", FRACTAL_CLIENT_ID!);
-  url.searchParams.append("redirect_uri", REDIRECT_URL);
-  url.searchParams.append("ensure_wallet", wallet);
-  url.searchParams.append("response_type", "code");
-  url.searchParams.append(
-    "scope",
-    "contact:read verification.basic:read verification.basic.details:read verification.liveness:read verification.liveness.details:read verification.wallet-eth:read"
-  );
-
-  return NextResponse.redirect(url);
 }
+ 
