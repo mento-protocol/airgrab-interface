92c521f77b6e6e7437a97d6119405348860b71bc
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-10-23 16:17:44 +0200

    refactor (merkle): improve error handling

--- src/utils/merkle.ts
+++ src/utils/merkle.ts
@@ -2,46 +2,75 @@ import fs from "fs";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
 let tree: StandardMerkleTree<any[]> | null = null;
-let loadError: Error | null = null;
 
-try {
-  tree = StandardMerkleTree.load(
-    JSON.parse(fs.readFileSync("src/merkle/tree.json", "utf8"))
-  );
-} catch (err) {
-  loadError = err as Error;
+function loadTree() {
+  if (!tree) {
+    try {
+      tree = StandardMerkleTree.load(
+        JSON.parse(fs.readFileSync("src/merkle/tree.json", "utf8"))
+      );
+    } catch (err) {
+      throw new Error(
+        `Error loading tree: ${
+          (err as Error).message
+        }. Make sure tree.json is present in the src/merkle directory.'`
+      );
+    }
+  } else {
+    return tree;
+  }
 }
-export const getTree = () => {
-  if (loadError) throw loadError;
+
+function initialize() {
+  loadTree();
+}
+
+export const getTree = (): StandardMerkleTree<any[]> | null => {
   return tree;
 };
 
-export const getAllocationList = (tree: StandardMerkleTree<any[]> | null) => {
-  if (!tree || loadError) throw loadError;
+export const getAllocationList = (
+  tree: StandardMerkleTree<any[]> | null
+): { [key: string]: string } => {
+  try {
+    if (!tree) throw new Error("Tree not found");
 
-  const allocationObject: { [key: string]: string } = {};
+    const allocationObject: { [key: string]: string } = {};
+    for (let [, [address, allocation]] of tree.entries()) {
+      allocationObject[address] = allocation;
+    }
 
-  for (let [, [address, allocation]] of tree.entries()) {
-    allocationObject[address] = allocation;
+    return allocationObject;
+  } catch (err) {
+    throw new Error(
+      `Error: merkle tree not found ${
+        (err as Error).message
+      }. Make sure tree.json is present in the src/merkle directory.'`
+    );
   }
-
-  return allocationObject;
 };
 
 export const getProofForAddress = (
   address: string,
   tree: StandardMerkleTree<any[]> | null
-) => {
-  if (!tree || loadError) throw loadError;
-
-  let proof;
-
-  for (const [i, v] of tree.entries()) {
-    if (v[0].toLowerCase() === address.toLowerCase()) {
-      proof = tree.getProof(i);
-      break;
+): string[] | undefined => {
+  if (!tree) throw new Error("Tree not found");
+  try {
+    let proof;
+    for (const [i, v] of tree.entries()) {
+      if (v[0].toLowerCase() === address.toLowerCase()) {
+        proof = tree.getProof(i);
+        break;
+      }
     }
+    return proof;
+  } catch (err) {
+    throw new Error(
+      `Error: merkle tree not found  ${
+        (err as Error).message
+      }. Make sure tree.json is present in the src/merkle directory.'`
+    );
   }
-
-  return proof;
 };
+
+initialize();
