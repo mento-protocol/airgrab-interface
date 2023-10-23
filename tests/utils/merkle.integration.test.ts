55a397dc31fb2d11abb0ef417e6055ab2180f3a3
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-10-23 16:29:01 +0200

    refactor: update test folder structure + convert to typescript

--- tests/utils/merkle.integration.test.ts
+++ tests/utils/merkle.integration.test.ts
@@ -1,16 +1,19 @@
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import { getAllocationList, getProof } from "@/utils/merkle.ts";
+import { getAllocationList, getProofForAddress } from "@/utils/merkle";
 import { parse } from "csv-parse/sync";
 import { TextEncoder } from "util";
 
+const USER = "0xA838E871A02C6d883bF004352Fc7dAc8f781FEd6";
+
 const csvData = parse(
-  new TextEncoder()
-    .encode(`0xA838E871A02C6d883bF004352Fc7dAc8f781FEd6,100000000000000000000
+  new TextEncoder().encode(`${USER},100000000000000000000
 0x6efD5DBb3E3C96C4c8bc7376AAe263D1C0e7F532,100000000000000000000
-0x70f2Db6D22593a65Dbb848C94eBACDF0344642F7,100000000000000000000`)
+0x70f2Db6D22593a65Dbb848C94eBACDF0344642F7,100000000000000000000`) as
+    | string
+    | Buffer
 );
 
-let tree;
+let tree: StandardMerkleTree<any[]> | null;
 
 beforeAll(() => {
   tree = StandardMerkleTree.of(csvData, ["address", "uint256"]);
@@ -19,15 +22,13 @@ beforeAll(() => {
 describe("getAllocationList", () => {
   test("should return correct allocation object", () => {
     const allocations = getAllocationList(tree);
-    expect(allocations["0xA838E871A02C6d883bF004352Fc7dAc8f781FEd6"]).toBe(
-      "100000000000000000000"
-    );
+    expect(allocations[USER]).toBe("100000000000000000000");
   });
 });
 
-describe("getProof", () => {
+describe("getProofForAddress", () => {
   test("should return a valid proof for an address", () => {
-    const proofForAddress = getProof(
+    const proofForAddress = getProofForAddress(
       "0xA838E871A02C6d883bF004352Fc7dAc8f781FEd6",
       tree
     );
@@ -35,7 +36,7 @@ describe("getProof", () => {
   });
 
   test("should return undefined for an invalid address", () => {
-    const proofForAddress = getProof("Invalid Address", tree);
+    const proofForAddress = getProofForAddress("Invalid Address", tree);
     expect(proofForAddress).toBeUndefined();
   });
 });
