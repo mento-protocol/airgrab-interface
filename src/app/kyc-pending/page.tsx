cedd1fc185a8dfcdfad70c891c26e985a86afa8f
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-11-07 15:26:21 +0100

    feat: add kyc rejected and kyc pending pages

---
+++ src/app/kyc-pending/page.tsx
@@ -0,0 +1,24 @@
+import { PrimaryButton } from "@/components/button";
+import Link from "next/link";
+
+const FRACTAL_APP_URL = process.env.NEXT_PUBLIC_FRACTAL_APP_URL;
+
+if (!FRACTAL_APP_URL) {
+  throw new Error("FRACTAL_APP_URL not found. Set 'FRACTAL_APP_URL' env var");
+}
+
+export default function KYCPending() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <span className="text-lg text-center flex flex-col">
+        Your KYC Verification is pending, for an update on your verification
+        status visit your{" "}
+        <Link className="text-primary-blue" href={FRACTAL_APP_URL!}>
+          Fractal ID Dashboard
+        </Link>
+      </span>
+
+      <PrimaryButton href="/">Return to Home</PrimaryButton>
+    </div>
+  );
+}
---
+++ src/app/kyc-rejected/page.tsx
@@ -0,0 +1,25 @@
+import { PrimaryButton } from "@/components/button";
+import Link from "next/link";
+
+const FRACTAL_APP_URL = process.env.NEXT_PUBLIC_FRACTAL_APP_URL;
+
+if (!FRACTAL_APP_URL) {
+  throw new Error("FRACTAL_APP_URL not found. Set 'FRACTAL_APP_URL' env var");
+}
+
+export default function KYCRejected() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <div className="flex flex-col items-center gap-6">
+        <span className="text-lg">
+          Your KYC Verification been rejected visit your
+        </span>{" "}
+        <Link className="text-primary-blue" href={FRACTAL_APP_URL!}>
+          Fractal ID dashboard
+        </Link>{" "}
+        for more details
+      </div>
+      <PrimaryButton href="/">Return to Home</PrimaryButton>
+    </div>
+  );
+}
