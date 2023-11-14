52fdf1ddaafcd550ccdb02e6dac914a8eaa35fcf
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-11-14 17:04:06 +0300

    feat: fix loading page spacing

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import MentoLoadingAnimation from "./mento-loading-animation";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 px-12">
       <h3 className="font-fg font-medium text-2xl">Loading... Please wait</h3>
       <MentoLoadingAnimation />
     </div>
