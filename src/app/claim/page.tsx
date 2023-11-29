e1e363b6f560726ef19c613d5d845a9f943db606
Bayological <6872903+bayological@users.noreply.github.com>
2023-11-29 18:22:06 -0500

    fix: fix width for tertiary button

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -10,89 +10,94 @@ import Link from "next/link";
 import React from "react";
 
 export default function Claim() {
-  const [hasClaimed, setHasClaimed] = React.useState(false);
-  const { allocation, session, isSessionLoading } = useAuthorization();
-  const { proof, signMessage } = useKYCProof({ enabled: true });
+   const [hasClaimed, setHasClaimed] = React.useState(false);
+   const { allocation, session, isSessionLoading } = useAuthorization();
+   const { proof, signMessage } = useKYCProof({ enabled: true });
 
-  const claim = () => {
-    setHasClaimed(true);
-  };
+   const claim = () => {
+      setHasClaimed(true);
+   };
 
-  if (!session.isKycVerified && !isSessionLoading) {
-    return <RedirectTo path="/" />;
-  }
+   if (!session.isKycVerified && !isSessionLoading) {
+      return <RedirectTo path="/" />;
+   }
 
-  if (!proof) {
-    return (
-      <div className="flex flex-col gap-8 items-center justify-center text-center">
-        <h3 className="font-fg font-medium text-2xl">
-          To claim your MENTO, you need to sign a message the verify your
-          Fractal KYC based on your wallet is included in your KYC verification.
-        </h3>
-        <p>
-          We use this signature to verify your KYC on chain with our partner
-          Fractal ID.
-        </p>
-        <PrimaryButton onClick={() => signMessage()}>
-          Sign a Message
-        </PrimaryButton>
-        <EligibilityFAQLink />
-      </div>
-    );
-  }
+   if (!proof) {
+      return (
+         <div className="flex flex-col gap-8 items-center justify-center text-center">
+            <h3 className="font-fg font-medium text-2xl">
+               To claim your MENTO, you need to sign a message the verify your
+               Fractal KYC based on your wallet is included in your KYC
+               verification.
+            </h3>
+            <p>
+               We use this signature to verify your KYC on chain with our
+               partner Fractal ID.
+            </p>
+            <PrimaryButton onClick={() => signMessage()}>
+               Sign a Message
+            </PrimaryButton>
+            <EligibilityFAQLink />
+         </div>
+      );
+   }
 
-  if (hasClaimed) {
-    return (
+   if (hasClaimed) {
+      return (
+         <div className="flex items-center justify-center flex-col gap-8 text-center md:px-40">
+            <Locked className="h-[248px] w-[251px]" />
+            <span className="font-fg font-medium text-2xl">
+               You claimed and locked{" "}
+               <span className="font-medium font-fg">{allocation} MNTO</span>{" "}
+               for <span className="font-medium font-fg">24 months</span>{" "}
+            </span>
+            <PrimaryButton internal href="/" fullWidth>
+               Check another wallet
+            </PrimaryButton>
+            <TertiaryButton
+               href="https://app.mento.org"
+               fullWidth
+               innerClassNames="md:w-[229px]"
+            >
+               Go to app
+            </TertiaryButton>
+         </div>
+      );
+   }
+
+   return (
       <div className="flex items-center justify-center flex-col gap-8 text-center md:px-20">
-        <Locked className="h-[248px] w-[251px]" />
-        <span className="font-fg font-medium text-2xl">
-          You claimed and locked{" "}
-          <span className="font-medium font-fg">{allocation} MNTO</span> for{" "}
-          <span className="font-medium font-fg">24 months</span>{" "}
-        </span>
-        <PrimaryButton internal href="/" fullWidth>
-          Check another wallet
-        </PrimaryButton>
-        <TertiaryButton href="https://app.mento.org" fullWidth>
-          Go to app
-        </TertiaryButton>
+         <p className="flex flex-col gap-8 font-fg">
+            <span className="text-xl">
+               You are eligible to claim{" "}
+               <span className="font-medium inline-block md:inline">
+                  {allocation} MNTO
+               </span>
+            </span>
+            <span className="text-xl">
+               To claim your MNTO, you are required to lock them as veMNTO for
+               24 months.{" "}
+               <span className="inline-block md:inline">
+                  You can&apos;t withdraw, but you can participate in governance
+                  of the protocol and receiving rewards.
+               </span>
+            </span>
+         </p>
+         <PrimaryButton fullWidth onClick={() => claim()}>
+            Claim & Lock {allocation} MNTO
+         </PrimaryButton>
+         <LockingFAQLink />
       </div>
-    );
-  }
-
-  return (
-    <div className="flex items-center justify-center flex-col gap-8 text-center md:px-20">
-      <p className="flex flex-col gap-8 font-fg">
-        <span className="text-xl">
-          You are eligible to claim{" "}
-          <span className="font-medium inline-block md:inline">
-            {allocation} MNTO
-          </span>
-        </span>
-        <span className="text-xl">
-          To claim your MNTO, you are required to lock them as veMNTO for 24
-          months.{" "}
-          <span className="inline-block md:inline">
-            You can&apos;t withdraw, but you can participate in governance of
-            the protocol and receiving rewards.
-          </span>
-        </span>
-      </p>
-      <PrimaryButton fullWidth onClick={() => claim()}>
-        Claim & Lock {allocation} MNTO
-      </PrimaryButton>
-      <LockingFAQLink />
-    </div>
-  );
+   );
 }
 
 const LockingFAQLink = () => {
-  return (
-    <Link
-      className="text-primary-blue underline font-fg text-xl"
-      href="#lock-tokens"
-    >
-      Why do I need to lock tokens?
-    </Link>
-  );
+   return (
+      <Link
+         className="text-primary-blue underline font-fg text-xl"
+         href="#lock-tokens"
+      >
+         Why do I need to lock tokens?
+      </Link>
+   );
 };
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,110 +1,113 @@
 import { ReactNode } from "react";
+import cn from "classnames";
 
 type BaseProps = {
-  children: ReactNode;
-  icon?: ReactNode;
-  target?: string;
-  rel?: string;
-  fullWidth?: boolean;
-  className?: string;
-  internal?: boolean;
+   children: ReactNode;
+   icon?: ReactNode;
+   target?: string;
+   rel?: string;
+   fullWidth?: boolean;
+   className?: string;
+   internal?: boolean;
+   innerClassNames?: string;
 };
 
 type ButtonActionProps =
-  | { href?: string; onClick?: () => void }
-  | { href?: undefined; onClick?: (() => void) | undefined };
+   | { href?: string; onClick?: () => void }
+   | { href?: undefined; onClick?: (() => void) | undefined };
 
 type ButtonProps = BaseProps & ButtonActionProps;
 
 type ColorProps = {
-  color: "blush" | "blue";
+   color: "blush" | "blue";
 };
 
 const BaseButton = ({
-  children,
-  icon,
-  href,
-  target = "",
-  rel = "",
-  color,
-  fullWidth,
-  internal,
-  className,
-  ...restProps
+   children,
+   icon,
+   href,
+   target = "",
+   rel = "",
+   color,
+   fullWidth,
+   internal,
+   className,
+   innerClassNames,
+   ...restProps
 }: ButtonProps & Partial<ColorProps>) => {
-  const isLink = Boolean(href);
-  const Component: any = isLink ? "a" : "button";
+   const isLink = Boolean(href);
+   const Component: any = isLink ? "a" : "button";
 
-  const isBlue = color === "blue";
+   const isBlue = color === "blue";
 
-  const containerClasses = [
-    "group",
-    "font-inter",
-    "outline-offset-4",
-    "cursor-pointer",
-    "border-b",
-    "rounded-lg",
-    "border-primary-dark",
-    "font-medium",
-    "select-none",
-    "inline-block",
-    href ? "inline-block" : "",
-    isBlue ? "bg-[#2A326A]" : "bg-[#845F84]",
-    fullWidth ? "w-full" : "",
-  ].filter(Boolean);
+   const containerClasses = [
+      "group",
+      "font-inter",
+      "outline-offset-4",
+      "cursor-pointer",
+      "border-b",
+      "rounded-lg",
+      "border-primary-dark",
+      "font-medium",
+      "select-none",
+      "inline-block",
+      href ? "inline-block" : "",
+      isBlue ? "bg-[#2A326A]" : "bg-[#845F84]",
+      fullWidth ? "w-full" : "",
+   ].filter(Boolean);
 
-  const innerClasses = [
-    "pl-10",
-    "group-active:-translate-y-[2px]",
-    "block",
-    "py-[18px]",
-    "group-hover:brightness-110",
-    "group-active:brightness-90",
-    "transition-transform",
-    "delay-[250]",
-    "hover:-translate-y-[6px]",
-    "-translate-y-[4px]",
-    "font-medium",
-    "text-[15px]",
-    "border",
-    "rounded-lg",
-    "border-primary-dark",
-    "leading-5",
-    icon ? "pr-6" : "pr-10",
-    isBlue
-      ? "bg-[#4D62F0] text-clean-white"
-      : "bg-primary-blush text-primary-dark",
-    fullWidth ? "w-full flex items-center justify-center" : "",
-  ].filter(Boolean);
+   const innerClasses = [
+      "pl-10",
+      "group-active:-translate-y-[2px]",
+      "block",
+      "py-[18px]",
+      "group-hover:brightness-110",
+      "group-active:brightness-90",
+      "transition-transform",
+      "delay-[250]",
+      "hover:-translate-y-[6px]",
+      "-translate-y-[4px]",
+      "font-medium",
+      "text-[15px]",
+      "border",
+      "rounded-lg",
+      "border-primary-dark",
+      "leading-5",
+      icon ? "pr-6" : "pr-10",
+      isBlue
+         ? "bg-[#4D62F0] text-clean-white"
+         : "bg-primary-blush text-primary-dark",
+      fullWidth ? "w-full flex items-center justify-center" : "",
+   ].filter(Boolean);
 
-  const contentClasses = ["flex", "items-center", icon ? "gap-3" : ""].filter(
-    Boolean
-  );
+   const contentClasses = ["", "items-center", icon ? "gap-3" : ""].filter(
+      Boolean
+   );
 
-  return (
-    <Component
-      href={href}
-      target={isLink && !internal ? target : undefined}
-      rel={isLink && !internal ? rel : undefined}
-      className={fullWidth ? "w-full md:w-auto" : ""}
-      {...restProps}
-    >
-      <span className={containerClasses.join(" ")}>
-        <span className={innerClasses.join(" ")}>
-          <span className={contentClasses.join(" ")}>
-            {children}
-            {icon}
-          </span>
-        </span>
-      </span>
-    </Component>
-  );
+   return (
+      <Component
+         href={href}
+         target={isLink && !internal ? target : undefined}
+         rel={isLink && !internal ? rel : undefined}
+         className={fullWidth ? "w-full md:w-auto" : ""}
+         {...restProps}
+      >
+         <span className={containerClasses.join(" ")}>
+            <span className={cn(innerClasses.join(" "), innerClassNames)}>
+               <span className={contentClasses.join(" ")}>
+                  {children}
+                  {icon}
+               </span>
+            </span>
+         </span>
+      </Component>
+   );
 };
 
 export const PrimaryButton = (props: ButtonProps) => (
-  <BaseButton {...props} color="blue" />
+   <BaseButton {...props} color="blue" />
 );
 export const SecondaryButton = (props: ButtonProps) => (
-  <BaseButton {...props} color="blush" />
+   <BaseButton {...props} color="blush" />
 );
 export const TertiaryButton = (props: ButtonProps) => <BaseButton {...props} />;
