e1862ce870cbe75d4c522f7185d943bf5ec55db5
Bayological <6872903+bayological@users.noreply.github.com>
2023-11-30 21:56:50 -0500

    refactor: center connect wallet heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,44 +14,44 @@ import Loading from "@/components/loading";
 // I think it would be more readable to move if (signature) check above, then check for if (!address) and remove the last last if (address) and just return
 
 export default function Home() {
-  const { address, isReconnecting } = useAccount();
-  const { signMessage, data: signature } = useAuthorization();
+   const { address, isReconnecting } = useAccount();
+   const { signMessage, data: signature } = useAuthorization();
 
-  const hasMounted = useHasMounted();
+   const hasMounted = useHasMounted();
 
-  if (!hasMounted || isReconnecting) {
-    return <Loading />;
-  }
+   if (!hasMounted || isReconnecting) {
+      return <Loading />;
+   }
 
-  if (signature) {
-    redirect("/allocation");
-  }
+   if (signature) {
+      redirect("/allocation");
+   }
 
-  if (!address) {
-    return (
-      <div className="flex flex-col gap-8 items-center justify-center">
-        <h3 className="font-fg font-medium text-2xl">
-          Connect wallet to check your eligibility to claim MNTO token
-        </h3>
-        <ConnectButton />
-        <EligibilityFAQLink />
+   if (!address) {
+      return (
+         <div className="flex flex-col gap-8 items-center justify-center text-center">
+            <h3 className="font-fg font-medium text-2xl">
+               Connect wallet to check your eligibility to claim MNTO token
+            </h3>
+            <ConnectButton />
+            <EligibilityFAQLink />
+         </div>
+      );
+   }
+
+   return (
+      <div className="flex flex-col gap-8 items-center justify-center text-center">
+         <h3 className="font-fg font-medium text-2xl">
+            To get started, please sign a message with your wallet.
+         </h3>
+         <p>
+            We use this to verify if you are the owner of the wallet & to check
+            your wallet's KYC status with our partner Fractal ID.
+         </p>
+         <PrimaryButton onClick={() => signMessage()}>
+            Sign a Message
+         </PrimaryButton>
+         <EligibilityFAQLink />
       </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col gap-8 items-center justify-center text-center">
-      <h3 className="font-fg font-medium text-2xl">
-        To get started, please sign a message with your wallet.
-      </h3>
-      <p>
-        We use this to verify if you are the owner of the wallet & to check your
-        wallet's KYC status with our partner Fractal ID.
-      </p>
-      <PrimaryButton onClick={() => signMessage()}>
-        Sign a Message
-      </PrimaryButton>
-      <EligibilityFAQLink />
-    </div>
-  );
+   );
 }
