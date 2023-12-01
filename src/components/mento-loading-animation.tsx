33932da384e4ecd7280766b9d62e711268a32069
Bayological <6872903+bayological@users.noreply.github.com>
2023-12-01 15:23:19 -0500

    fix: adjust loading screen for mobile

--- src/components/mento-loading-animation.tsx
+++ src/components/mento-loading-animation.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { MentoIcon } from "./svgs";
 
 const MentoLoadingAnimation = () => {
-  return (
-    <div className="w-[124px] h-[124px] mx-auto my-6">
-      <MentoIcon className="animate-spin-slow" />
-    </div>
-  );
+   return (
+      <div className="w-[124px] h-[124px] mx-auto sm:my-6 md:my-6">
+         <MentoIcon className="animate-spin-slow" />
+      </div>
+   );
 };
 
 export default MentoLoadingAnimation;
