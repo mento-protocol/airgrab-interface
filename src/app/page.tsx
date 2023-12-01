b59a28c324cacafb5e00ffcda61aa38f964cf8c0
Bayological <6872903+bayological@users.noreply.github.com>
2023-12-01 17:08:30 -0500

    fix: correct token name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       return (
          <div className="flex flex-col gap-8 items-center justify-center text-center">
             <h3 className="font-fg font-medium text-[18px] md:text-base">
-               Connect wallet to check your eligibility to claim MNTO token
+               Connect wallet to check your eligibility to claim MENTO token
             </h3>
             <ConnectButton color="blue" />
             <EligibilityFAQLink />
