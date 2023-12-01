e2a70ed065964ee5a630f11d7dceb9398e4f2c3a
Bayological <6872903+bayological@users.noreply.github.com>
2023-12-01 15:01:51 -0500

    fix: blue connect button on main card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             <h3 className="font-fg font-medium text-[18px] md:text-base">
                Connect wallet to check your eligibility to claim MNTO token
             </h3>
-            <ConnectButton />
+            <ConnectButton color="blue" />
             <EligibilityFAQLink />
          </div>
       );
