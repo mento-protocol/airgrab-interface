1ca85a0767c7182ec9fbfc516b18aad73a2123f9
Bayological <6872903+bayological@users.noreply.github.com>
2023-12-04 09:39:53 -0500

    refactor: remove unneeded styling

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -62,7 +62,7 @@ const AccordianMenu = ({
                            id={id}
                            className={`${
                               open ? "border-b" : ""
-                           }  p-[1rem] pl-8 sm:p-[1.875rem] md:p-[1.875rem] sm:text-base md:text-base mt-2 font-medium text-left text-sm font-fg flex justify-between w-full`}
+                           }  p-[1rem] pl-8 sm:p-[1.875rem] md:p-[1.875rem] sm:text-base mt-2 font-medium text-left text-sm font-fg flex justify-between w-full`}
                         >
                            {question}
 
