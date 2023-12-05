247c3dc9af6a798a961e50337b4d591af109afe6
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-12-05 14:12:10 +0300

    refactor(session): remove sign in button (handled by rainbowkit now)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,37 @@
 import React from "react";
 import { ConnectButton } from "@/components/connect-button";
 import RedirectTo from "@/components/redirect-to";
-import { SignInButton } from "@/components/sign-in-button";
 import { useAuthorization } from "@/contexts/authorization-provider.client";
 import { useAccount } from "wagmi";
 import { EligibilityFAQLink } from "@/components/eligibility-faq-link";
 
 export default function Home() {
-   const { isLoggedIn, isSessionLoading } = useAuthorization();
-   const { address, isConnecting } = useAccount();
+  const { isSessionLoading } = useAuthorization();
+  const { address, isConnecting } = useAccount();
 
-   if (isConnecting) {
-      return <>loading...</>;
-   }
+  if (isConnecting) {
+    return <>loading...</>;
+  }
 
-   if (!address) {
-      return (
-         <div className="flex flex-col gap-8 items-center justify-center text-center">
-            <h3 className="font-fg font-medium text-[18px] md:text-base">
-               Connect wallet to check your eligibility to claim MENTO token
-            </h3>
-            <ConnectButton color="blue" />
-            <EligibilityFAQLink />
-         </div>
-      );
-   }
+  if (!address) {
+    return (
+      <div className="flex flex-col gap-8 items-center justify-center text-center">
+        <h3 className="font-fg font-medium text-[18px] md:text-base">
+          Connect wallet to check your eligibility to claim MENTO token
+        </h3>
+        <ConnectButton color="blue" />
+        <EligibilityFAQLink />
+      </div>
+    );
+  }
 
-   if (!isLoggedIn) {
-      // Sign in button with dummy error and success handlers
-      return <SignInButton />;
-   }
+  if (isSessionLoading) {
+    return (
+      <div className="flex flex-col gap-8">
+        <h3 className="font-fg font-medium text-2xl">Signing In</h3>
+      </div>
+    );
+  }
 
-   if (isSessionLoading) {
-      return (
-         <div className="flex flex-col gap-8">
-            <h3 className="font-fg font-medium text-2xl">Signing In</h3>
-         </div>
-      );
-   }
-
-   return <RedirectTo path="/allocation" />;
+  return <RedirectTo path="/allocation" />;
 }
