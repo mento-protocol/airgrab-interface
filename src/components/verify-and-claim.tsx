3d4869c3b4aa9ce064b2126108e98840ee0b81ca
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-12-07 14:36:19 +0300

    feat(claim): update verify and claim flow

--- src/components/verify-and-claim.tsx
+++ src/components/verify-and-claim.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useClaimMento } from "@/hooks/use-claim-mento";
-import { useKYCProof } from "@/hooks/use-kyc-proof";
 import React from "react";
-import { PrimaryButton } from "./button";
+import { PrimaryButton, TertiaryButton } from "./button";
 import Link from "next/link";
 import { shortenAddress } from "@/lib/addresses";
+import { Locked } from "./svgs";
 
 const LockingFAQLink = () => {
   return (
@@ -13,6 +13,16 @@ const LockingFAQLink = () => {
       href="#lock-tokens"
     >
       Why do I need to lock tokens?
+    </Link>
+  );
+};
+const KYCFAQLink = () => {
+  return (
+    <Link
+      className="text-primary-blue underline font-fg text-xl"
+      href="#verify"
+    >
+      Why is this requird?
     </Link>
   );
 };
@@ -26,68 +36,147 @@ export default function VerifyAndClaim({
   allocation: string;
   merkleProof: string[] | undefined;
 }) {
-  const { verificationDetails, signMessage, hasSignedMessage } = useKYCProof();
-  const canClaim = Boolean(address && verificationDetails);
   const shortAddress = address ? shortenAddress(address) : "";
 
-  const { claim } = useClaimMento({
-    enabled: canClaim,
-    merkleProof,
+  const { prepare, claim, kyc } = useClaimMento({
     allocation,
+    merkleProof,
     address,
-    kycDetails: verificationDetails as any,
   });
-
-  if (!hasSignedMessage) {
+  const renderOverview = () => {
+    if (kyc.isSuccess && claim.write) {
+      return <ClaimAndLockOverview />;
+    }
+    if (kyc.isLoadingSignature || kyc.isLoadingProof) {
+      return <KYCOverview />;
+    }
     return (
-      <div className="flex items-center justify-center flex-col gap-8 text-center">
-        <h3 className="font-fg font-medium text-sm sm:text-base text-center flex flex-col gap-8">
-          <span>
-            Congratulations, wallet address{" "}
-            <span className="text-primary-blue">{shortAddress}</span> is
-            elligible to receive
-          </span>
-          <span className="text-base sm:text-2xl">{allocation} MENTO</span>
-        </h3>
-        <p className="flex flex-col gap-8 font-fg">
-          <span className="text-sm sm:text-xl">
-            To claim your MENTO, sign a message to verify your Fractal KYC.{" "}
-            <br className="hidden sm:block" />
-            This signature helps us confirm your KYC on-chain with Fractal ID.
-          </span>
-        </p>
-
-        <PrimaryButton onClick={() => signMessage()}>
-          Claim & Lock {allocation} MENTO
-        </PrimaryButton>
-        <LockingFAQLink />
-      </div>
+      <ClaimOverview shortAddress={shortAddress} allocation={allocation} />
     );
-  }
+  };
+
+  const renderButton = () => {
+    if (kyc.isSuccess && claim.write) {
+      return (
+        <PrimaryButton onClick={() => claim?.write!()}>
+          {claim.isLoading ? (
+            <>Continue in wallet</>
+          ) : (
+            <>Claim & Lock {allocation} MENTO</>
+          )}
+        </PrimaryButton>
+      );
+    }
+    return (
+      <PrimaryButton onClick={() => kyc.signMessage()}>
+        {kyc.isLoadingSignature ? (
+          <>Continue in wallet</>
+        ) : kyc.isLoadingProof ? (
+          <>Loading...</>
+        ) : (
+          <>Claim & Lock {allocation} MENTO</>
+        )}
+      </PrimaryButton>
+    );
+  };
 
   return (
     <div className="flex items-center justify-center flex-col gap-8 text-center">
-      <h3 className="font-fg font-medium text-sm sm:text-base text-center flex flex-col gap-8">
-        <span>
-          Congratulations, wallet address{" "}
-          <span className="text-primary-blue">{address}</span> is elligible to
-          receive
-        </span>
-        <span className="text-base sm:text-2xl">{allocation} MENTO</span>
-      </h3>
-      <p className="flex flex-col gap-8 font-fg">
-        <span className="text-sm sm:text-xl">
-          To claim your MENTO, you are required to lock them as veMENTO for{" "}
-          <br className="hidden sm:block" />
-          24 months. You can&apos;t withdraw, but you can participate in{" "}
-          <br className="hidden sm:block" />
-          governance of the protocol and receiving rewards.
-        </span>
-      </p>
-      <PrimaryButton onClick={() => claim!()}>
-        Claim & Lock {allocation} MENTO
-      </PrimaryButton>
-      <LockingFAQLink />
+      {claim.claimed ? (
+        <Claimed allocation={allocation} />
+      ) : (
+        <>
+          {renderOverview()}
+          {renderButton()}
+        </>
+      )}
     </div>
   );
 }
+
+const ClaimOverview = ({
+  shortAddress,
+  allocation,
+}: {
+  shortAddress: string;
+  allocation: string;
+}) => (
+  <>
+    <ClaimHeading>
+      <span>
+        Congratulations, wallet address{" "}
+        <span className="text-primary-blue">{shortAddress}</span> is elligible
+        to receive
+      </span>
+      <span className="text-base sm:text-2xl">{allocation} MENTO</span>
+    </ClaimHeading>
+    <ClaimDescription>
+      <span className="text-sm sm:text-xl">
+        To claim your MENTO, you are required to lock them as veMENTO for{" "}
+        <br className="hidden sm:block" />
+        24 months. You can&apos;t withdraw, but you can participate in{" "}
+        <br className="hidden sm:block" />
+        governance of the protocol and receiving rewards.
+      </span>
+    </ClaimDescription>
+  </>
+);
+
+const KYCOverview = () => (
+  <>
+    <ClaimHeading>Sign to confirm KYC</ClaimHeading>
+    <ClaimDescription>
+      <span className="text-sm sm:text-xl">
+        To claim your MENTO, sign a message to verify your Fractal KYC.{" "}
+        <br className="hidden sm:block" />
+        This signature helps us confirm your KYC on-chain with Fractal ID.
+      </span>
+      <KYCFAQLink />
+    </ClaimDescription>
+  </>
+);
+const ClaimAndLockOverview = () => (
+  <>
+    <ClaimHeading>Claim & Lock MENTO</ClaimHeading>
+    <ClaimDescription>
+      <span className="text-sm sm:text-xl">
+        To claim your MENTO, you are required to lock them as veMENTO for{" "}
+        <br className="hidden sm:block" />
+        24 months. You can&apos;t withdraw, but you can participate in{" "}
+        <br className="hidden sm:block" />
+        governance of the protocol and receiving rewards.
+      </span>
+      <LockingFAQLink />
+    </ClaimDescription>
+  </>
+);
+
+const Claimed = ({ allocation }: { allocation: string }) => (
+  <div className="flex items-center justify-center flex-col gap-8 text-center px-20">
+    <Locked className="h-[248px] w-[251px]" />
+    <span className="font-fg font-normal text-sm sm:text-xl">
+      You claimed and locked <br className="block sm:hidden" />
+      <span className="font-medium font-fg">{allocation} MENTO</span> for{" "}
+      <span className="font-medium font-fg">24 months</span>{" "}
+    </span>
+    <div className="flex flex-col gap-[18px]">
+      <PrimaryButton internal href="/" fullWidth>
+        Check another wallet
+      </PrimaryButton>
+      <TertiaryButton href="https://app.mento.org" fullWidth>
+        Go to app
+      </TertiaryButton>
+    </div>
+  </div>
+);
+
+const ClaimHeading = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <h3 className="font-fg font-medium text-sm sm:text-base text-center flex flex-col gap-8">
+      {children}
+    </h3>
+  );
+};
+const ClaimDescription = ({ children }: { children: React.ReactNode }) => {
+  return <p className="flex flex-col gap-8 font-fg">{children}</p>;
+};
