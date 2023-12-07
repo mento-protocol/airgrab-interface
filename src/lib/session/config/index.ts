bdde25dfd1ccf2c3fdb57c08101967515c908240
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-12-07 21:59:18 +0300

    feat: expire session in 2 hours

--- src/lib/session/config/index.ts
+++ src/lib/session/config/index.ts
@@ -16,5 +16,7 @@ export const sessionOptions: SessionOptions = {
     // secure only works in `https` environments
     // if your localhost is not on `https`, then use: `secure: process.env.NODE_ENV === "production"`
     secure: process.env.NODE_ENV === "production",
+    maxAge: 7200,
+    httpOnly: true,
   },
 };
