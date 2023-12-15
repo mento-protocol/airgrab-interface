d30be9d060a41c92e42112d1803f7647fabd127f
Philip Paetz <philip.paetz@me.com>
2023-12-15 15:44:21 +0100

    chore: fix typo merkel => merkle

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -1,23 +1,23 @@
+import VerifyAndClaim from "@/components/verify-and-claim";
 import {
   getAllocationForAddress,
   getProofForAddress,
   getTree,
 } from "@/lib/merkle/merkle";
 import { getAddressForSession, getServerSession } from "@/lib/session";
-import VerifyAndClaim from "@/components/verify-and-claim";
 import { formatUnits } from "viem";
 
 export default async function Claim() {
   const session = await getServerSession();
   const fullAddress = getAddressForSession(session);
   const allocation = getAllocationForAddress(fullAddress);
-  const merkelProof = getProofForAddress(fullAddress, getTree());
+  const merkleProof = getProofForAddress(fullAddress, getTree());
 
   return (
     <VerifyAndClaim
       allocation={formatUnits(BigInt(allocation ?? 0), 18)}
       address={fullAddress}
-      merkleProof={merkelProof}
+      merkleProof={merkleProof}
     />
   );
 }
