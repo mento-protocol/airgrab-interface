6099278137a944f9b7103ee215cdd818b9403bb4
Philip Paetz <philip.paetz@me.com>
2023-12-15 13:38:12 +0100

    chore: fix node and path issues in buildTree.js

--- src/lib/merkle/buildTree.mjs
+++ src/lib/merkle/buildTree.mjs
@@ -1,11 +1,19 @@
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import { parse } from "csv-parse/sync";
-import fs from "fs";
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
 
 export const buildMerkleTreeFromCSV = () => {
-  const values = parse(fs.readFileSync("list.csv", { encoding: "utf8" }), {
-    columns: false,
-  });
+  const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const values = parse(
+    fs.readFileSync(path.resolve(__dirname, "./list.csv"), {
+      encoding: "utf8",
+    }),
+    {
+      columns: false,
+    }
+  );
 
   const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
 
