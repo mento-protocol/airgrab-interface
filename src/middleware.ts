eea2614a9a88e941e6a788ee0a68537571e684a7
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-12-20 19:31:09 +0000

    feat(middleware): Update middleware logic to account for launch date

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getServerSession } from "./lib/session";
+import { LAUNCH_DATE } from "./lib/constants";
 
 export const config = {
   matcher: ["/claim/:path*", "/allocation/:path*", "/"],
@@ -8,13 +9,14 @@ export const config = {
 
 export async function middleware(request: NextRequest) {
   const session = await getServerSession();
-
+  const hasLaunchStarted = LAUNCH_DATE.getTime() > Date.now();
+  const redirectUrl = hasLaunchStarted ? "/claim" : "/alloacation";
   const isHomePage = request.nextUrl.pathname === "/";
   const isClaimPage = request.nextUrl.pathname.startsWith("/claim");
   const hasSession = session?.siwe?.success;
 
-  if (!isClaimPage && session?.isKycVerified) {
-    return NextResponse.redirect(new URL("/claim", request.url));
+  if (!redirectUrl && session?.isKycVerified) {
+    return NextResponse.redirect(new URL(redirectUrl, request.url));
   }
 
   if (isClaimPage && !session?.isKycVerified) {
