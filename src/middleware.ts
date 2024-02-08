7ce8b7f350ddeec014f7d6a0f8d97f942215961b
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-02-08 16:38:57 -0500

    refactor: simplify middleware auth checking

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,30 +9,33 @@ export const config = {
 
 export async function middleware(request: NextRequest) {
   const session = await getServerSession();
-  const hasLaunchStarted = new Date(LAUNCH_DATE).getTime() < Date.now();
-  const redirectUrl = hasLaunchStarted ? "/claim" : "/allocation";
+  const isPastLaunchDate = new Date(LAUNCH_DATE).getTime() < Date.now();
   const isHomePage = request.nextUrl.pathname === "/";
   const isClaimPage = request.nextUrl.pathname.startsWith("/claim");
+  const isAllocationPage = request.nextUrl.pathname.startsWith("/allocation");
   const hasSession = session?.siwe?.success;
-  const isRedirectUrl = request.nextUrl.pathname === redirectUrl;
 
-  if (!isClaimPage && session.hasClaimed) {
-    return NextResponse.redirect(new URL("/claim", request.url));
-  }
-
-  if (!isRedirectUrl && session?.isKycVerified) {
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
-  }
-
-  if (isClaimPage && !session?.isKycVerified && !session.hasClaimed) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
+  // every route excpet home requires a session
   if (!isHomePage && !hasSession) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
+  // Redirect to allocation page if user is on home page and has a session
   if (isHomePage && hasSession) {
     return NextResponse.redirect(new URL("/allocation", request.url));
+  }
+
+  // Redirect to claim page if user is on allocation page and has a session and is past launch date and is kyc verified or has claimed
+  if (
+    isAllocationPage &&
+    isPastLaunchDate &&
+    (session?.isKycVerified || session.hasClaimed)
+  ) {
+    return NextResponse.redirect(new URL("/claim", request.url));
+  }
+
+  if (isClaimPage && !session?.isKycVerified && !session.hasClaimed) {
+    console.log({ session: session.hasClaimed });
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
