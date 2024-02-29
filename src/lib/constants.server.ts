422fb8b4f346b6263ad013a3b472f9660f68b25a
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-02-29 14:30:08 -0500

    refactor(missing env var error): improve error message accuracy

--- src/lib/constants.server.ts
+++ src/lib/constants.server.ts
@@ -2,30 +2,30 @@ import "server-only";
 
 if (!process.env.MAILCHIMP_SERVER_PREFIX) {
   throw new Error(
-    "MAILCHIMP_SERVER_PREFIX is not set. Please set MAILCHIMP_SERVER_PREFIX environment variable.",
+    "MAILCHIMP_SERVER_PREFIX is undefined. Please set MAILCHIMP_SERVER_PREFIX environment variable.",
   );
 }
 if (!process.env.MAILCHIMP_API_KEY) {
   throw new Error(
-    "MAILCHIMP_API_KEY is not set. Please set MAILCHIMP_API_KEY environment variable.",
+    "MAILCHIMP_API_KEY is undefined. Please set MAILCHIMP_API_KEY environment variable.",
   );
 }
 if (!process.env.MAILCHIMP_AUDIENCE_ID) {
   throw new Error(
-    "MAILCHIMP_AUDIENCE_ID is not set. Please set MAILCHIMP_AUDIENCE_ID environment variable.",
+    "MAILCHIMP_AUDIENCE_ID is undefined. Please set MAILCHIMP_AUDIENCE_ID environment variable.",
   );
 }
 if (!process.env.FRACTAL_CLIENT_SECRET)
   throw new Error(
-    "FRACTAL_CLIENT_SECRET is not set. Please set FRACTAL_CLIENT_SECRET environment variable.",
+    "FRACTAL_CLIENT_SECRET is undefined. Please set FRACTAL_CLIENT_SECRET environment variable.",
   );
 if (!process.env.SECRET_COOKIE_PASSWORD) {
   throw new Error(
-    "SECRET_COOKIE_PASSWORD is not set, please set in environment variables",
+    "SECRET_COOKIE_PASSWORD is undefined, please set in environment variables",
   );
 }
 if (!process.env.FRACTAL_KYC_LEVEL) {
-  throw new Error("FRACTAL_KYC_LEVEL is not set");
+  throw new Error("FRACTAL_KYC_LEVEL is undefined");
 }
 
 // Exporting the variables
