669959385557f4e5730e19781fe406f3ae7b8017
Philip Paetz <philip.paetz@me.com>
2024-03-25 18:11:05 +0100

    chore: selectively allow non-dangerous "SECRET" strings

--- src/scripts/get-secrets.sh
+++ src/scripts/get-secrets.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################
 # Script for pulling secret keys from GCP secret manager.
-# Usage: ./bin/get-secrets.sh 
+# Usage: ./bin/get-secrets.sh
 ##############################################################################
 
 # Authenticate with Google Cloud
@@ -20,20 +20,21 @@ echo $ENV_FILE
 for SECRET_ID in $(echo "$SECRET_IDS" | tr ',' ' '); do
   # Retrieve the secret value from Google Cloud
   SECRET_NAME="$(echo "$SECRET_ID" | tr '-' '_' | tr '[:lower:]' '[:upper:]')"
+  # secretlint-disable-next-line
   SECRET_VALUE="$(gcloud secrets versions access latest --secret="$SECRET_ID" --project="$PROJECT_ID")"
 
   # Write the secret value to the .env file
-  
-  # If the secret name already exists in the .env file, replace the value with the new value. 
+
+  # If the secret name already exists in the .env file, replace the value with the new value.
   if grep -q "^$SECRET_NAME=" "$ENV_FILE"; then
     sed -i "" "s/^$SECRET_NAME=.*/$SECRET_NAME=$SECRET_VALUE/g" "$ENV_FILE"
   else
     # If the .env file is not empty, append the secret name and value to the .env file.
     if [[ -s "$ENV_FILE" ]]; then
-      printf "\n%s=%s" "$SECRET_NAME" "$SECRET_VALUE" >> "$ENV_FILE"
+      printf "\n%s=%s" "$SECRET_NAME" "$SECRET_VALUE" >>"$ENV_FILE"
     else
       # If we don't have an .env, write the secret name and value to the .env file.
-      echo -n "$SECRET_NAME=$SECRET_VALUE" >> "$ENV_FILE"
+      echo -n "$SECRET_NAME=$SECRET_VALUE" >>"$ENV_FILE"
     fi
   fi
 done
