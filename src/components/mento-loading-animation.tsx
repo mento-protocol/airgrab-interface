5fcceed0f44d4568b45eec738f37a02e9f4d0cc6
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-05-10 14:54:58 +0200

    fix: Small tweaks to improve layout shift + loading

--- src/components/mento-loading-animation.tsx
+++ src/components/mento-loading-animation.tsx
@@ -4,7 +4,7 @@ import { MentoIcon } from "./svgs";
 
 const MentoLoadingAnimation = () => {
   return (
-    <div className="sm:w-[124px] sm:h-[124px] mx-auto sm:my-6 md:my-6 ">
+    <div className="sm:w-[124px] sm:h-[124px] mx-auto sm:my-6 md:my-0">
       <MentoIcon className="animate-spin-slow w-[64px] sm:min-w-full" />
     </div>
   );
