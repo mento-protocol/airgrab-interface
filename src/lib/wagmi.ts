c1f8f039c56b99e826969133dcb91008d367d2db
Ryan Noble <ryanjnoble@gmail.com>
2024-05-14 11:46:16 +0200

    feat: added frame

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -3,6 +3,7 @@ import {
   metaMaskWallet,
   omniWallet,
   walletConnectWallet,
+  frameWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import { configureChains, createConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
@@ -33,6 +34,7 @@ const connectors = connectorsForWallets([
       metaMaskWallet({ chains, projectId }),
       omniWallet({ chains, projectId }),
       walletConnectWallet({ chains, projectId }),
+      frameWallet({ chains, projectId }),
     ],
   },
 ]);
