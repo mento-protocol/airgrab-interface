352602ecd6cf0f51a0af9df75e70697871f9f280
baroooo <baranseltekin@gmail.com>
2024-05-28 16:50:43 +0200

    feat: update wording

--- src/app/kyc-pending/page.tsx
+++ src/app/kyc-pending/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/button";
 import { FRACTAL_APP_URL } from "@/lib/constants";
-import Link from "next/link";
 
 export default function KYCPending({
   searchParams,
@@ -17,15 +16,11 @@ export default function KYCPending({
       </h3>
       <span className="text-lg text-center flex flex-col">
         {contacted
-          ? "Your KYC Verification is pending, you have been contacted by Fractal ID for more information. For an update please check your email or visit your"
-          : "Your KYC Verification is pending; for an update on your verification status, visit your"}{" "}
-        <Link className="text-primary-blue" href={FRACTAL_APP_URL}>
-          Fractal ID Dashboard
-        </Link>
+          ? "Your KYC Verification is pending, you have been contacted by Fractal ID for more information. For an update please check your email or visit your dashboard"
+          : "Fractal ID team is reviewing the documents you have uploaded. You can check current status in the dashboard"}
       </span>
-
-      <Button color="blue" href="/">
-        Return to Home
+      <Button target="_blank" color="blue" href={FRACTAL_APP_URL}>
+        Go to Fractal ID
       </Button>
     </div>
   );
