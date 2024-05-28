33d3d5443b4583464c62de90f2ddb370eccf59f2
baroooo <baranseltekin@gmail.com>
2024-05-28 17:12:47 +0200

    feat: add dots

--- src/app/kyc-pending/page.tsx
+++ src/app/kyc-pending/page.tsx
@@ -16,8 +16,8 @@ export default function KYCPending({
       </h3>
       <span className="text-lg text-center flex flex-col">
         {contacted
-          ? "Your KYC Verification is pending, you have been contacted by Fractal ID for more information. For an update please check your email or visit your dashboard"
-          : "Fractal ID team is reviewing the documents you have uploaded. You can check current status in the dashboard"}
+          ? "Your KYC Verification is pending, you have been contacted by Fractal ID for more information. For an update please check your email or visit your dashboard."
+          : "Fractal ID team is reviewing the documents you have uploaded. You can check current status in the dashboard."}
       </span>
       <Button target="_blank" color="blue" href={FRACTAL_APP_URL}>
         Go to Fractal ID
